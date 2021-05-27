2a0bcc1c21d47a2e363351d12ecbc97581b906a0
Martin <Martin@V.cz>
2021-05-27 22:56:00 +0200

    mvc

--- SemesterAssignment/src/main/java/cz/tul/Main.java
+++ SemesterAssignment/src/main/java/cz/tul/Main.java
@@ -1,6 +1,7 @@
 package cz.tul;
 
 
+import cz.tul.controller.AppController;
 import cz.tul.data.Country;
 import cz.tul.data.Measurement;
 import cz.tul.data.Town;
@@ -9,13 +10,15 @@ import cz.tul.service.MeasurementService;
 import cz.tul.service.TownService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ("cz.tul"))
 @EnableJpaRepositories("cz.tul.repositories")
 @EnableMongoRepositories("cz.tul.repositories")
 public class Main {
